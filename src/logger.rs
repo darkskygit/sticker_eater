8b540663f705491459b380d12b66014070fafffd
DarkSky <darksky2048@gmail.com>
2021-02-06 18:50:40 +0800

    cleanup logs

--- src/logger.rs
+++ src/logger.rs
@@ -31,11 +31,7 @@ pub struct Logger;
 
 impl log::Log for Logger {
     fn enabled(&self, metadata: &Metadata) -> bool {
-        if !cfg!(debug_assertions) {
-            true
-        } else {
-            metadata.level() <= Level::Info
-        }
+        metadata.level() <= Level::Info
     }
 
     fn log(&self, record: &Record) {
